ac931062805c98d5a39e8aca401a6eb962d5b0c3
the1andonlymanojos <mshivagange@gmail.com>
2026-04-14 02:44:50 +0530

    fixed null error on create new user

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -65,6 +65,7 @@ public class User implements UserDetails {
         user.setUserId(UUID.randomUUID());
 
         user.setCreatedAt(Instant.now());
+        user.setAdminRequest(false);
 
         return user;
     }
